f4d135bdc0a30b2ffd354683348aec00ef886edb
puya310 <puya310r@gmail.com>
2023-07-30 20:07:21 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+
+  required_version = ">= 0.15"
+}
+
+provider "aws" {
+  profile = "default"
+  region  =  var.aws_region
+}
+
+
+########################
+# Bucket creation
+########################
+resource "aws_s3_bucket" "my_protected_bucket" {
+  bucket = var.bucket_name
+}
+
+##########################
+# Bucket private access
+##########################
+resource "aws_s3_bucket_acl" "my_protected_bucket_acl" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+  acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+
+
+
+#############################
+# Enable bucket versioning
+#############################
+resource "aws_s3_bucket_versioning" "my_protected_bucket_versioning" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+
+########################
+# Disabling bucket
+# public access
+########################
+resource "aws_s3_bucket_public_access_block" "my_protected_bucket_access" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+
+  # Block public access
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
