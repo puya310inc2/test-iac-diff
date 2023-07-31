c44da6bafc42472d4e94cb35487571ef1938f78c
puya310 <puya310r@gmail.com>
2023-07-30 20:07:48 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+  default     = "puya-s3-tf-provisioned"
+
+}
